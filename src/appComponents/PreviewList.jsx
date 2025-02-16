729ac51aec8203380d06f493ee2f280b87fb754c
River Wang <urfdvw@gmail.com>
2025-02-16 16:10:19 -0500

    auto scroll in preview

--- src/appComponents/PreviewList.jsx
+++ src/appComponents/PreviewList.jsx
@@ -1,22 +1,34 @@
 import AppContext from "../AppContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { PreviewVerseBox } from "./VerseBox";
+import { scroller, Element } from "react-scroll";
 
 export default function PreviewList() {
     const { getChapterVerses, previewVerse } = useContext(AppContext);
     const verses = getChapterVerses(previewVerse.book, previewVerse.chapter);
+
+    useEffect(() => {
+        const targetName = `preview-verse-${previewVerse.verse}`;
+        scroller.scrollTo(targetName, {
+            duration: 800,
+            delay: 0,
+            smooth: "easeInOutQuart",
+            containerId: "previewContainer",
+        });
+    }, [previewVerse]);
     return (
-        <>
+        <div id="previewContainer" style={{ height: "100%", overflowY: "auto" }}>
             {verses.map((verseVersions) => {
                 return (
-                    <PreviewVerseBox
-                        book={verseVersions[0].book}
-                        chapter={verseVersions[0].chapter}
-                        verse={verseVersions[0].verse}
-                        key={verseVersions[0].text}
-                    />
+                    <Element key={verseVersions[0].verse} name={`preview-verse-${verseVersions[0].verse}`}>
+                        <PreviewVerseBox
+                            book={verseVersions[0].book}
+                            chapter={verseVersions[0].chapter}
+                            verse={verseVersions[0].verse}
+                        />
+                    </Element>
                 );
             })}
-        </>
+        </div>
     );
 }
